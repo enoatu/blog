ccc21d52c600e0037f486ae3031d97ec5dc1c7af
enoatu <acro1452@gmail.com>
2021-09-02 08:12:35 +0900

    :wrench: modify html.js

--- src/html.js
+++ src/html.js
@@ -32,7 +32,7 @@ export default function HTML(props) {
             __html: `
               <!-- Google Tag Manager (noscript) -->
               <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P46GWR4"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
               <!-- End Google Tag Manager (noscript) -->
             `,
           }}
