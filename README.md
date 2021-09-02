0e877bddc7348cc4f2e17aa35eeb3f853cd445ad
enoatu <acro1452@gmail.com>
2021-09-02 15:24:17 +0900

    :wrench: add status

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # blog
+[![Netlify Status](https://api.netlify.com/api/v1/badges/ece45645-48a4-47c5-833b-09f9e08c48a6/deploy-status)](https://app.netlify.com/sites/blog-enoatu/deploys)
 ## compose
 - contentful + netlify + Gatsby
 ## devlop
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect, useRef } from 'react'
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import get from 'lodash/get'
@@ -8,44 +9,64 @@ import Layout from '@/components/layout'
 import heroStyles from '@/components/hero.module.css'
 import postStyles from '@/templates/blog-post.module.css'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = get(this.props, 'data.contentfulBlogPost')
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+export default (props) => {
+  const post = get(props, 'data.contentfulBlogPost')
+  const siteTitle = get(props, 'data.site.siteMetadata.title')
+  const markdownBodyRef = useRef(null)
+  useEffect(() => {
+    markdownBodyRef.current.load = () => {
+      document
+        .querySelectorAll(['data-click-count=1'])
+        .forEach((clickCountEl) => {
+          clickCountEl.addEventListener(
+            'click',
+            () => {
+              window.ga(
+                'send',
+                'event',
+                'banner',
+                'click',
+                clickCountEl.href,
+                1
+              )
+            },
+            { once: true }
+          )
+        })
+    }
+  }, [])
 
-    return (
-      <Layout location={this.props.location}>
-        <div className={postStyles.layoutInner}>
-          <Helmet title={`${post.title} | ${siteTitle}`} />
-          <div className={heroStyles.hero}>
-            <Img
-              className={heroStyles.heroImage}
-              alt={post.title}
-              fluid={post.heroImage.fluid}
-            />
-          </div>
-          <div className="wrapper">
-            <h1 className="section-headline">{post.title}</h1>
-            <p
-              style={{
-                display: 'block',
-              }}
-            >
-              {post.publishDate}
-            </p>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: post.body.childMarkdownRemark.html,
-              }}
-            />
-          </div>
+  return (
+    <Layout location={props.location}>
+      <div className={postStyles.layoutInner}>
+        <Helmet title={`${post.title} | ${siteTitle}`} />
+        <div className={heroStyles.hero}>
+          <Img
+            className={heroStyles.heroImage}
+            alt={post.title}
+            fluid={post.heroImage.fluid}
+          />
         </div>
-      </Layout>
-    )
-  }
+        <div className="wrapper">
+          <h1 className="section-headline">{post.title}</h1>
+          <p
+            style={{
+              display: 'block',
+            }}
+          >
+            {post.publishDate}
+          </p>
+          <div
+            ref={markdownBodyRef}
+            dangerouslySetInnerHTML={{
+              __html: post.body.childMarkdownRemark.html,
+            }}
+          />
+        </div>
+      </div>
+    </Layout>
+  )
 }
-
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
