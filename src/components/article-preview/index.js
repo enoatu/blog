c2e423c65e68d683fb07304ac2d1dde14150ae2d
enoatu <acro1452@gmail.com>
2021-09-04 15:08:25 +0900

    :wrench: mv dir

---
+++ src/components/article-preview/index.js
@@ -0,0 +1,18 @@
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+import styles from './index.module.css'
+
+export default ({ article }) => (
+  <div className={styles.preview}>
+    <Img alt="" fluid={article.heroImage.fluid} />
+    <h3 className={styles.previewTitle}>
+      <Link to={`/blog/${article.slug}`}>{article.title}</Link>
+    </h3>
+    <small>{article.publishDate}</small>
+    <p
+      dangerouslySetInnerHTML={{
+        __html: article.description.childMarkdownRemark.html,
+      }}
+    />
+  </div>
+)
