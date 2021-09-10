f9f314638546880991867cddb9b883a41009d00b
enoatu <acro1452@gmail.com>
2021-09-10 18:17:27 +0900

    :wrench: font-size em to rem

--- typography-theme-enotiru/index.js
+++ typography-theme-enotiru/index.js
@@ -35,7 +35,25 @@ const theme = {
       WebkitFontSmoothing: 'antialiased',
     },
     'h1,h2,h3,h4,h5,h6': {
-      letterSpacing: '0.02em',
+      letterSpacing: '0.02rem',
+    },
+    h1: {
+      fontSize: '2rem',
+    },
+    h2: {
+      fontSize: '1.8rem',
+    },
+    h3: {
+      fontSize: '1.6rem',
+    },
+    h4: {
+      fontSize: '1.4rem',
+    },
+    h5: {
+      fontSize: '1.2rem',
+    },
+    'h4,h5,h6,a,p,small,li,strong': {
+      fontSize: '1rem',
     },
     p: {
       textAlign: 'justify',
@@ -48,21 +66,21 @@ const theme = {
       color: '#3498DB',
     },
     li: {
-      marginBottom: '0.5em',
+      marginBottom: '0.5rem',
     },
     blockquote: {
       color: '#333',
       fontSize: '85%',
       position: 'relative',
-      margin: '0.5em',
-      padding: '0.5em 1em 0.5em 2em',
+      margin: '0.5rem',
+      padding: '0.5rem 1rem 0.5rem 2rem',
     },
     'blockquote:before': {
       position: 'absolute',
-      fontSize: '1.5em',
+      fontSize: '1.5rem',
       lineHeight: '1',
-      top: '0.2em',
-      left: '0.5em',
+      top: '0.2rem',
+      left: '0.5rem',
       content: '"\\201C"',
       color: '#333',
     },
