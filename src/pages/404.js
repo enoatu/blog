2241f1185dacf6deeebe2a9715af896485db014f
enoatu <acro1452@gmail.com>
2021-09-10 22:39:33 +0900

    :wrench: modify 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,16 @@
-const NotFound = () => {
-  return <div>404 Not Found</div>
+import Helmet from 'react-helmet'
+import Layout from '@c/layout'
+const NotFound = (props) => {
+  return (
+    <Layout location={props.location}>
+      <Helmet>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+      <h1>404 Not Found ページがみつかりません</h1>
+      <p>
+        ページが移動したかもしれません。↑のナビゲーションからページを移動してほしいです。
+      </p>
+    </Layout>
+  )
 }
 export default NotFound
